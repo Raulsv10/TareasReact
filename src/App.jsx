03fb5625ae73ec849ed009df0a1929fc063d7b58
Raúl Sánchez <raul.sv05@gmail.com>
2025-04-09 18:20:34 -0600

    Fix typo in Navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import NavBar from "./Components/Navbar";
+import NavBar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
